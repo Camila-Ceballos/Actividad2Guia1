9ab7d7e4c35a987df15ac1bf23755f5be7cc9afc
Camila Avendaño <25974211-CamilaAvendano@users.noreply.replit.com>
2024-08-19 23:25:08 +0000

    corrección menú

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,17 +12,17 @@ public class Main { // nombre de la clase
     System.out.println("|            1. Ejercicio 1           | ");
     System.out.println("|            2. Ejercicio 2           | ");
     System.out.println("|            3. Ejercicio 3           | ");
-    System.out.println("|            4. Ejercicio 3           | ");
-    System.out.println("|            5. Ejercicio 3           | ");
-    System.out.println("|            6. Ejercicio 3           | ");
-    System.out.println("|            7. Ejercicio 3           | ");
-    System.out.println("|            8. Ejercicio 3           | ");
-    System.out.println("|            9. Ejercicio 3           | ");
-    System.out.println("|           10. Ejercicio 3           | ");
-    System.out.println("|           11. Ejercicio 3           | ");
-    System.out.println("|           12. Ejercicio 3           | ");
-    System.out.println("|           13. Ejercicio 3           | ");
-    System.out.println("|           14. Ejercicio 3           | ");
+    System.out.println("|            4. Ejercicio 4           | ");
+    System.out.println("|            5. Ejercicio 5           | ");
+    System.out.println("|            6. Ejercicio 6           | ");
+    System.out.println("|            7. Ejercicio 7           | ");
+    System.out.println("|            8. Ejercicio 8           | ");
+    System.out.println("|            9. Ejercicio 9           | ");
+    System.out.println("|           10. Ejercicio 10          | ");
+    System.out.println("|           11. Ejercicio 11          | ");
+    System.out.println("|           12. Ejercicio 12          | ");
+    System.out.println("|           13. Ejercicio 13          | ");
+    System.out.println("|           14. Ejercicio 14          | ");
     System.out.println("+-------------------------------------+");
 
 
